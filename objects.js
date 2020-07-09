878657c1643e47b9bc6917667798f4ddffbaaaf9
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-09 15:20:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,6 @@ var playlist={
 var updatePlaylist= (playlist,key,value) =>{
   return Object.assign({},playlist,{[key]:value})
 }
+var removeFromPlaylist= (playlist,key) =>{
+  return Object.assign({},playlist,{[key]:})
+}
