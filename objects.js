e45175db32a38a4c46b781ffc3bdc6ea762bf139
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-09 15:22:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,8 @@ var playlist={
   SidhuMooseWala:["Dhakka","Famous"]
 }
 var updatePlaylist= (playlist,key,value) =>{
-  return Object.assign({},playlist,{[key]:value})
+  return Object.assign(playlist,{[key]:value})
 }
 var removeFromPlaylist= (playlist,key) =>{
-  return Object.assign({},playlist,{[key]:})
+  return Object.assign(playlist,{[key]:})
 }
